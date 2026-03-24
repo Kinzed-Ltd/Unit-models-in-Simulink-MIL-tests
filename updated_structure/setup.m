65fa78d83891fd6dbd738d730e205076302eba97
jy <jianfei.yu@kinzed.com>
2026-03-24 20:40:47 +0000

    Add updated_structure

---
+++ updated_structure/setup.m
@@ -0,0 +1,14 @@
+function setup
+
+currPath = fileparts(mfilename('fullpath'));
+addpath(currPath);
+
+cachePath = fullfile(currPath, 'sim_files', 'cache');
+codeGenPath = fullfile(currPath, 'sim_files', 'codeGen');
+
+Simulink.fileGenControl('set', 'CacheFolder', cachePath, ...
+   'CodeGenFolder', codeGenPath, 'createDir',true);
+
+open_system('Hybrid_vehicle')
+
+end
