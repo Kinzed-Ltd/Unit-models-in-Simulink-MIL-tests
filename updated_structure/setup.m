07bf40034ff12398a2b7469f3988b1837cfc2b1c
jy <jianfei.yu@kinzed.com>
2026-03-27 21:12:25 +0000

    Add unit models

--- updated_structure/setup.m
+++ updated_structure/setup.m
@@ -1,7 +1,9 @@
 function setup
 
 currPath = fileparts(mfilename('fullpath'));
+subPath = fullfile(currPath, 'sub_models');
 addpath(currPath);
+addpath(subPath);
 
 cachePath = fullfile(currPath, 'sim_files', 'cache');
 codeGenPath = fullfile(currPath, 'sim_files', 'codeGen');
@@ -9,6 +11,6 @@ codeGenPath = fullfile(currPath, 'sim_files', 'codeGen');
 Simulink.fileGenControl('set', 'CacheFolder', cachePath, ...
    'CodeGenFolder', codeGenPath, 'createDir',true);
 
-open_system('Hybrid_vehicle')
+open_system('Hybrid_vehicle_using_model_reference')
 
 end
